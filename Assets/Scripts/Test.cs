a4ef6476138282a969326b0ab6cd4541f7a3709e
RomainVivier <romain.simon.vivier@gmail.com>
2014-09-16 10:49:09 +0200

    Petit ajout d'un test pour renforcer le tuto

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+	void Start()
+	{
+		KeyBinder.Instance.DefineActions("AButton", new KeyActionConfig(KeyType.Action, 0,
+        	() => {
+				Debug.Log("A button click down");
+			},
+			() => {
+				Debug.Log("A button click up");
+			}));
+
+		KeyBinder.Instance.DefineActions("VerticalTest", new AxisActionConfig(KeyType.Action, 0, 
+			value =>{
+				Debug.Log("Vertical value = " + value);
+			} ));
+	}
+}
